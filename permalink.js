c5be71f9a36cdbefe2dc01b4e8d42441dd5f9bc7
Jacob van't Hoog <vantjac@gmail.com>
2016-04-26 19:12:38 -0700

    Clean up permalink code, higher resolution next button

--- permalink.js
+++ permalink.js
@@ -1,5 +1,5 @@
 var base64String = getRandomBase64();
-var permalink = window.location.href.split('?')[0]+ "?v=0&b=" + base64String;
+var permalink = window.location.href.split('?')[0] + "?v=0&b=" + base64String;
 
 permalinkElement = document.getElementById("permalink");
-permalinkElement.innerHTML = "<a href=\"" + permalink + "\">Permalink<a>";
+permalinkElement.setAttribute("href", permalink);
--- random.js
+++ random.js
@@ -1,5 +1,8 @@
+const FLOAT_SIZE = 4; //number of bytes in a float
+const MAX_RANDOM_NUMBERS = 1024;
+
 var randomCount = 0;
-var randomView = new DataView(new ArrayBuffer(4096));
+var randomView = new DataView(new ArrayBuffer(MAX_RANDOM_NUMBERS * FLOAT_SIZE));
 // if false, write random numbers to randomView
 // if true, read random numbers from randomView
 var readRandom = false;
@@ -13,34 +16,13 @@ if(getParameterByName("b") !=null) {
 
 function random() {
     var r;
-    if(!readRandom) {
-	r = Math.random();
-	randomView.setFloat32((randomCount++) * 4, r);
+    if(readRandom) {
+	r = randomView.getFloat32((randomCount++) * FLOAT_SIZE);
     } else {
-	r = randomView.getFloat32((randomCount++) * 4); 
+	r = Math.random();
+	randomView.setFloat32((randomCount++) * FLOAT_SIZE, r);
     }
     return r;
-}
-
-function getRandomBase64() {
-    var dataString = "";
-    for(var i = 0; i < randomCount * 4; i++) {
-	dataString += String.fromCharCode(randomView.getUint8(i));
-    }
-    var base64String = btoa(dataString);
-    
-    return base64String;
-}
-
-function loadRandomBase64(base64String) {
-    readRandom = true;
-    // +s are replaced with spaces in query strings
-    // replace all
-    base64String = base64String.split(" ").join("+");
-    var dataString = atob(base64String);
-    for(var i = 0; i < dataString.length; i++) {
-	randomView.setUint8(i, dataString.charCodeAt(i));
-    }
 }
 
 // max is exclusive
@@ -66,4 +48,26 @@ function getParameterByName(name, url) {
     if (!results) return null;
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+
+function getRandomBase64() {
+    var dataString = "";
+    for(var i = 0; i < randomCount * FLOAT_SIZE; i++) {
+	dataString += String.fromCharCode(randomView.getUint8(i));
+    }
+    var base64String = btoa(dataString);
+    
+    return base64String;
+}
+
+function loadRandomBase64(base64String) {
+    readRandom = true;
+    // +s are replaced with spaces in query strings
+    // replace all
+    base64String = base64String.split(" ").join("+");
+    var dataString = atob(base64String);
+    for(var i = 0; i < dataString.length; i++) {
+	randomView.setUint8(i, dataString.charCodeAt(i));
+    }
 }
